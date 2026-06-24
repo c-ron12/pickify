3a53ed9fafb1328bb180833f3dab97eda1376978
c-ron12 <pioneerboy912@gmail.com>
2026-06-24 17:12:01 +0545

    Refactor testimonial section to dynamically display user testimonials and add a submission form

--- resources/views/testimonial.blade.php
+++ resources/views/testimonial.blade.php
@@ -1,83 +1,34 @@
 @extends('layoutTemplate.main')
 @section('main-container')
 
-<section class="client_section layout_padding">
+<section class="client_section layout_padding" style="margin-top: 80px;">
     <div class="container">
         <div class="heading_container heading_center">
-            <h2>
-                Testimonial
-            </h2>
+            <h2>Testimonial</h2>
         </div>
     </div>
     <div class="container px-0">
-        <div id="customCarousel2" class="carousel  carousel-fade" data-ride="carousel">
+        <div id="customCarousel2" class="carousel carousel-fade" data-ride="carousel">
             <div class="carousel-inner">
-                <div class="carousel-item active">
+                @foreach ($testimonials as $index => $testimonial)
+                <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
                     <div class="box">
                         <div class="client_info">
                             <div class="client_name">
-                                <h5>
-                                    Morijorch
-                                </h5>
-                                <h6>
-                                    Default model text
-                                </h6>
+                                <h5>{{ $testimonial->user->name }}</h5>
+                                <h6>Registered User</h6>
+                                <div class="testimonial-rating-display">
+                                    @for ($i = 1; $i <= 5; $i++) <span
+                                        class="star {{ $testimonial->rating <= $i ? 'filled' : '' }}">★</span>
+                                        @endfor
+                                </div>
                             </div>
                             <i class="fa fa-quote-left" aria-hidden="true"></i>
                         </div>
-                        <p>
-                            editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will
-                            uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their
-                            default model text, and a search for 'lorem ipsum' will uncover many web sites still in
-                            their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search
-                            for 'lorem ipsum' will uncover many web sites still in their infancy. Various
-                        </p>
+                        <p>{{ $testimonial->content }}</p>
                     </div>
                 </div>
-                <div class="carousel-item">
-                    <div class="box">
-                        <div class="client_info">
-                            <div class="client_name">
-                                <h5>
-                                    Rochak
-                                </h5>
-                                <h6>
-                                    Default model text
-                                </h6>
-                            </div>
-                            <i class="fa fa-quote-left" aria-hidden="true"></i>
-                        </div>
-                        <p>
-                            Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem
-                            ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem
-                            Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web
-                            sites still in their infancy. editors now use Lorem Ipsum as their default model text, and a
-                            search for 'lorem ipsum' will uncover many web sites still in their infancy.
-                        </p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="box">
-                        <div class="client_info">
-                            <div class="client_name">
-                                <h5>
-                                    Brad Johns
-                                </h5>
-                                <h6>
-                                    Default model text
-                                </h6>
-                            </div>
-                            <i class="fa fa-quote-left" aria-hidden="true"></i>
-                        </div>
-                        <p>
-                            Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem
-                            ipsum' will uncover many web sites still in their infancy, editors now use Lorem Ipsum as
-                            their default model text, and a search for 'lorem ipsum' will uncover many web sites still
-                            in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a
-                            search for 'lorem ipsum' will uncover many web sites still in their infancy. Various
-                        </p>
-                    </div>
-                </div>
+                @endforeach
             </div>
             <div class="carousel_btn-box">
                 <a class="carousel-control-prev" href="#customCarousel2" role="button" data-slide="prev">
@@ -92,5 +43,40 @@
         </div>
     </div>
 </section>
+
+<!-- Testimonial form  -->
+<div class="testimonial-form-container">
+    <div class="testimonial-form-box">
+
+        @if(session('success'))
+        <div class="success-message">{{ session('success') }}</div>
+        @endif
+
+        <h2>Share Your Experience</h2>
+
+        <form action="/submit_testimonial" method="POST">
+            @csrf
+
+            <!-- Star Rating Field -->
+            <div class="star-rating">
+                <input type="radio" name="rating" id="star5" value="5"><label for="star5" title="5 stars">★</label>
+                <input type="radio" name="rating" id="star4" value="4"><label for="star4" title="4 stars">★</label>
+                <input type="radio" name="rating" id="star3" value="3"><label for="star3" title="3 stars">★</label>
+                <input type="radio" name="rating" id="star2" value="2"><label for="star2" title="2 stars">★</label>
+                <input type="radio" name="rating" id="star1" value="1"><label for="star1" title="1 star">★</label>
+            </div>
+
+            <!-- Testimonial Text -->
+            <div>
+                <textarea name="content" class="testimonial-textarea" placeholder="Write your testimonial here..."
+                    required></textarea>
+            </div>
+
+            <!-- Submit Button -->
+            <button type="submit" class="testimonial-submit-btn">Submit Testimonial</button>
+        </form>
+    </div>
+</div>
+
 <!-- end client section -->
 @endsection
