1a3d4fe5e2857eaca6a28eec35c93c19532f63c2
c-ron12 <pioneerboy912@gmail.com>
2026-06-24 17:13:37 +0545

    Update footer section with social media links, improved content, and updated contact information

--- resources/views/layoutTemplate/footer.blade.php
+++ resources/views/layoutTemplate/footer.blade.php
@@ -2,72 +2,59 @@
 <section class="info_section  layout_padding2-top">
     <div class="social_container">
         <div class="social_box">
-            <a href="">
-                <i class="fa fa-facebook" aria-hidden="true"></i>
+            <a href="https://www.facebook.com/PickifyStore" target="_blank" rel="noopener noreferrer">
+                <i class="fa fa-facebook" aria-hidden="true" id="facebook"></i>
             </a>
-            <a href="">
-                <i class="fa fa-twitter" aria-hidden="true"></i>
+            <a href="https://www.tiktok.com/@pickifystore12" target="_blank" rel="noopener noreferrer">
+                <i class="fa fa-tiktok" aria-hidden="true" id="tiktok"></i>
             </a>
-            <a href="">
-                <i class="fa fa-instagram" aria-hidden="true"></i>
-            </a>
-            <a href="">
-                <i class="fa fa-youtube" aria-hidden="true"></i>
+            <a href="https://www.instagram.com/pickifystore12/" target="_blank" rel="noopener noreferrer">
+                <i class="fa fa-instagram" aria-hidden="true" id="instagram"></i>
             </a>
         </div>
     </div>
     <div class="info_container ">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 col-lg-3">
+                <div class="col-md-4">
                     <h6>
                         ABOUT US
                     </h6>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
-                        consectetur
-                        adipiscing elit, sed doLorem ipsum dolor sit amet,
+                    <p class="footerPara1">
+                        Your favorite online store for trendy shoes and stylish clothing. We offer premium quality
+                        fashion, unbeatable prices, and a seamless shopping experience—so you can step out in
+                        confidence. Discover the latest trends, enjoy fast delivery, and exclusive deals just for you.
+                        Shop now and elevate your wardrobe with effortless style!
                     </p>
                 </div>
-                <div class="col-md-6 col-lg-3">
-                    <div class="info_form ">
-                        <h5>
-                            Newsletter
-                        </h5>
-                        <form action="#">
-                            <input type="email" name="email" placeholder="Enter your email">
-                            <button>
-                                Subscribe
-                            </button>
-                        </form>
-                    </div>
+
+                <div class="col-md-4">
+                    <h6 style="margin-bottom: 15px;">NEED HELP</h6>
+                    <ul class="list-unstyled">
+                        <li><a href="/faq">FAQs</a></li>
+                        <li><a href="/track-order">Track Your Order</a></li>
+                        <li><a href="/returns">Returns & Exchanges</a></li>
+                        <li><a href="/shipping-info">Shipping Information</a></li>
+                        <li><a href="/support">Customer Support</a></li>
+                    </ul>
                 </div>
-                <div class="col-md-6 col-lg-3">
-                    <h6>
-                        NEED HELP
-                    </h6>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
-                        consectetur
-                        adipiscing elit, sed doLorem ipsum dolor sit amet,
-                    </p>
-                </div>
-                <div class="col-md-6 col-lg-3">
+
+                <div class="col-md-4">
                     <h6>
                         CONTACT US
                     </h6>
                     <div class="info_link-box">
                         <a href="">
                             <i class="fa fa-map-marker" aria-hidden="true"></i>
-                            <span> Gb road 123 london Uk </span>
+                            <span>Narephat, Koteshwor-32, Kathmandu</span>
                         </a>
                         <a href="">
                             <i class="fa fa-phone" aria-hidden="true"></i>
-                            <span>+01 12345678901</span>
+                            <span>+977-9824063524</span>
                         </a>
                         <a href="">
                             <i class="fa fa-envelope" aria-hidden="true"></i>
-                            <span> demo@gmail.com</span>
+                            <span>pickifystore12@gmail.com</span>
                         </a>
                     </div>
                 </div>
@@ -78,7 +65,7 @@
     <footer class=" footer_section">
         <div class="footer__block block no-margin-bottom">
             <div class="container-fluid text-center">
-                <p class="no-margin-bottom">copyright &copy; pickify.com | All right reserved 2020 | Deploved by c-ron12
+                <p class="no-margin-bottom">copyright &copy; pickify.com | All right reserved 2025 | Deploved by c-ron12
                 </p>
             </div>
         </div>
