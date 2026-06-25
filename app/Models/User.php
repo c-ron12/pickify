4e2b299ea94f596a2c0024e13fbb5c253d52b212
c-ron12 <pioneerboy912@gmail.com>
2026-06-25 12:46:24 +0545

    Add testimonials relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable implements MustVerifyEmail
             'password' => 'hashed',
         ];
     }
+
+    public function testimonials()
+    {
+        return $this->hasMany(Testimonial::class);
+    }
 }
