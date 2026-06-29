df1c20a6dc575d566931ddf2c4fa2a42bfdda7a0
c-ron12 <pioneerboy912@gmail.com>
2026-06-29 14:55:38 +0545

    feat: add temporary returns, shipping and support blade file

---
+++ resources/views/returns.blade.php
@@ -0,0 +1,8 @@
+@extends('layoutTemplate.main')
+@section('main-container')
+
+<div class="container info-container my-5 text-center">
+    <h2 class="pt-5">Return and exchanges</h2>
+</div>
+
+@endsection
---
+++ resources/views/shipping.blade.php
@@ -0,0 +1,8 @@
+@extends('layoutTemplate.main')
+@section('main-container')
+
+<div class="container info-container my-5 text-center">
+    <h2 class="pt-5">Shipping information</h2>
+</div>
+
+@endsection
